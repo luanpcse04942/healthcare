1d66e9cec8c5bf88aae81b7b54904874898c5e49
Thienhdhe163728 <thienhdhe163728@fpt.edu.vn>
2022-09-25 21:47:53 +0700

    Status and appointment model

---
+++ OnlineHealthCare/src/java/com/laptrinhweb/healthcare/model/Status.java
@@ -0,0 +1,32 @@
+package com.laptrinhweb.healthcare.model;
+
+
+public class Status {
+    private int statusId;
+    private String statusName;
+
+    public Status() {
+    }
+
+    public Status(int statusId, String statusName) {
+        this.statusId = statusId;
+        this.statusName = statusName;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+    
+}
