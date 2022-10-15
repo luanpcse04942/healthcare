7babc6e6e7b6ae89f7e37c463e9dc58a11997a25
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2022-10-15 23:04:11 +0700

    Done Patient List

--- OnlineHealthCare/src/java/com/laptrinhweb/healthcare/services/PatientService.java
+++ OnlineHealthCare/src/java/com/laptrinhweb/healthcare/services/PatientService.java
@@ -4,12 +4,37 @@
  */
 package com.laptrinhweb.healthcare.services;
 
-import java.util.ArrayList;
+import com.laptrinhweb.healthcare.dao.PatientDAO;
+import com.laptrinhweb.healthcare.model.dto.PatientSmall;
+import java.util.List;
 
 /**
  *
  * @author LuanPC
  */
 public class PatientService {
-   
+       public List<PatientSmall> getListAccounts(int page, int doctorID) {
+        PatientDAO patientDAO = new PatientDAO();
+        int recordsPerPage = 6;
+        return patientDAO.findAllPatients((page - 1) * recordsPerPage, recordsPerPage, doctorID);    
+    }
+       
+       public int getNoOfPage(String search) {
+        PatientDAO patientDAO = new PatientDAO();
+        int noOfRecords = 0;
+        if(search.isEmpty()) 
+        {
+            noOfRecords = patientDAO.getNoOfRecordAccounts();
+        }else {
+            noOfRecords = patientDAO.getNoOfRecordAccountsSearch(search);
+        }
+        
+        //calculate number of page
+        int recordsPerPage = 6;
+        int noOfPages = noOfRecords / recordsPerPage;
+        if (noOfRecords % recordsPerPage > 0) {
+            noOfPages++;
+        }
+        return noOfPages;
+    }
 }
