34a453034de1452256779be4cefa41120bd59b14
MaiVietHungHE16355 <hungmvhe163655@fpt.edu.vn>
2022-10-15 23:04:11 +0700

    Done Patient List

--- OnlineHealthCare/src/java/com/laptrinhweb/healthcare/controller/PatientController.java
+++ OnlineHealthCare/src/java/com/laptrinhweb/healthcare/controller/PatientController.java
@@ -1,5 +1,7 @@
 package com.laptrinhweb.healthcare.controller;
 
+import com.laptrinhweb.healthcare.services.PatientService;
+import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,24 +13,38 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author LuanPC
  */
-@WebServlet(name="PatientController", urlPatterns={"/PatientController"})
+@WebServlet(name = "PatientController", urlPatterns = {"/doctor-patient-list", "/doctor-patient-search"})
 public class PatientController extends HttpServlet {
-   
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
-    } 
+            throws ServletException, IOException {
+        int page = 1;
+        String search = "";
+
+        if (request.getServletPath().equals("/doctor-patient-list")) {
+            if (request.getParameter("page") != null) {
+                page = Integer.parseInt(request.getParameter("page"));
+            }
+            PatientService patientService = new PatientService();
+
+            request.setAttribute("accounts", patientService.getListAccounts(page, 1));
+            request.setAttribute("noOfPages", patientService.getNoOfPage(search));
+            request.setAttribute("currentPage", page);
+            RequestDispatcher rd = request.getRequestDispatcher("Doctor/PatientList.jsp");
+            rd.forward(request, response);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
-    
+
 }
--- OnlineHealthCare/src/java/com/laptrinhweb/healthcare/dao/PatientDAO.java
+++ OnlineHealthCare/src/java/com/laptrinhweb/healthcare/dao/PatientDAO.java
@@ -1,14 +1,16 @@
 package com.laptrinhweb.healthcare.dao;
 
 import com.laptrinhweb.healthcare.context.DBContext;
-import com.laptrinhweb.healthcare.model.Time;
 import com.laptrinhweb.healthcare.model.User;
+import com.laptrinhweb.healthcare.model.dto.PatientSmall;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,7 +19,7 @@ import java.util.List;
 public class PatientDAO extends DBContext {
 
     public List<User> getListPatient(int id) {
-        String sql = "select u.id, firstName, lastName, email, gender, phoneNumber, address \n"
+        String sql = "select u.id, firstName, lastName, gender, phoneNumber, address \n"
                 + "from Users u, User_Profile up, Appointment a\n"
                 + "where up.userId = u.id \n"
                 + "and a.statusId = 3 \n"
@@ -36,10 +38,8 @@ public class PatientDAO extends DBContext {
                 acc.setId(rs.getInt(1));
                 acc.setFirstName(rs.getNString(2));
                 acc.setLastName(rs.getNString(3));
-                acc.setEmail(rs.getNString(4));
-                acc.setGender(rs.getNString(5));
-                acc.setPhoneNumber(rs.getString(6));
-                acc.setAddress(rs.getNString(7));
+                acc.setPhoneNumber(rs.getString(4));
+                acc.setAddress(rs.getNString(5));
                 listAccount.add(acc);
             }
         } catch (SQLException e) {
@@ -75,5 +75,141 @@ public class PatientDAO extends DBContext {
         } catch (SQLException e) {
         }
         return null;
+    }
+
+    public List<PatientSmall> findAllPatients(int start, int total, int id) {
+        String sql = "select u.id, firstName, lastName, phoneNumber, a.bookingDate\n"
+                + "from Users u, User_Profile up, Appointment a\n"
+                + "where up.userId = u.id \n"
+                + "and a.statusId = 3 \n"
+                + "and a.userId = u.id \n"
+                + "and doctorWorkingInfoId = " + id + "\n"
+                + "ORDER BY a.userId OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        DBContext db = new DBContext();
+        List<PatientSmall> listAccount = new ArrayList<>();
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, start);
+            ps.setInt(2, total);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                PatientSmall acc = new PatientSmall();
+                acc.setId(rs.getInt(1));
+                acc.setFirstName(rs.getNString(2));
+                acc.setLastName(rs.getNString(3));
+                acc.setPhoneNumber(rs.getString(4));
+                acc.setBookingDate(rs.getDate(5));
+                listAccount.add(acc);
+            }
+        } catch (SQLException e) {
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return listAccount;
+    }
+
+    public int getNoOfRecordAccounts() {
+        String query = "select count(*) \n"
+                + "from Users u, User_Profile up, Appointment a\n"
+                + "where up.userId = u.id \n"
+                + "and a.statusId = 3 \n"
+                + "and a.userId = u.id \n"
+                + "and doctorWorkingInfoId = 1";
+        DBContext db = new DBContext();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return count;
+    }
+
+    public int getNoOfRecordAccountsSearch(String str) {
+        String query = "select count(*) \n"
+                + "from Users u, User_Profile up, Appointment a\n"
+                + "where up.userId = u.id \n"
+                + "and a.statusId = 3 \n"
+                + "and a.userId = u.id \n"
+                + "and doctorWorkingInfoId = 1";
+        DBContext db = new DBContext();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DoctorDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return count;
     }
 }
