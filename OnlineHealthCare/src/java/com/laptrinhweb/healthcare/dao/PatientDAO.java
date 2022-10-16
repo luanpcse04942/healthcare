73e71f029cad6bbdb8ee42ab8130781f53108f16
DamNhat <mikuotngokm@gmail.com>
2022-10-16 22:12:08 +0700

    create patient appointment list

--- OnlineHealthCare/src/java/com/laptrinhweb/healthcare/dao/PatientDAO.java
+++ OnlineHealthCare/src/java/com/laptrinhweb/healthcare/dao/PatientDAO.java
@@ -3,6 +3,7 @@ package com.laptrinhweb.healthcare.dao;
 import com.laptrinhweb.healthcare.context.DBContext;
 import com.laptrinhweb.healthcare.model.User;
 import com.laptrinhweb.healthcare.model.dto.PatientSmall;
+import dto.AppointmentPatient;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -262,6 +263,132 @@ public class PatientDAO extends DBContext {
         return listAccount;
     }
 
+    public ArrayList<AppointmentPatient> getAllAppointmentPatient(int start, int total) {
+        StringBuilder sql = new StringBuilder("select a.appointmentId,d.fullName,a.reasonExamination,addr.address,a.bookingDate,s.statusName from Appointment a ");
+        sql.append(" join Users b on b.id = a.userId ");
+        sql.append(" join Doctor_Working_Info c on c.id = a.doctorWorkingInfoId ");
+        sql.append("  join (select id,CONCAT(firstName,' ',lastName) as fullName from Users) d on d.id = c.doctorId");
+        sql.append(" join ( select m.id,p.address from Users u join MedicalFacilityInfo m on m.medicalFacilityId = u.id join User_Profile p on p.userId = u.id) addr on addr.id = c.medicalFacilityInfoId ");
+        sql.append(" join Status s on s.statusId = a.statusId ORDER BY b.id OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        DBContext db = new DBContext();
+        ArrayList<AppointmentPatient> listAppointment = new ArrayList<>();
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(sql.toString());
+            ps.setInt(1, start);
+            ps.setInt(2, total);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                AppointmentPatient app = new AppointmentPatient();
+                app.setAppoitmentId(rs.getInt(1));
+                app.setFullName(rs.getString(2));
+                app.setReason(rs.getString(3));
+                app.setAddress(rs.getString(4));
+                app.setBookingDate(rs.getDate(5));
+                app.setStatus(rs.getString(6));
+                listAppointment.add(app);
+            }
+        } catch (SQLException e) {
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+            }
+        }
+        return listAppointment;
+    }
+
+    public int getNoOfRecordAppointment() {
+        String query = "SELECT count(*) FROM Appointment";
+        DBContext db = new DBContext();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return count;
+    }
+
+    public int getNoOfRecordAppointmentSeach(String search) {
+        String query = "SELECT count(*) FROM Appointment a join (select u.id ,(convert(VARCHAR(255), firstName)+' '+convert(VARCHAR(255), lastName)) as name from Users u) t on t.id = a.userId where t.name like '%" + search + "%'";
+        DBContext db = new DBContext();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int count = 0;
+        try {
+            conn = db.getConn();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, search);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                count = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PatientDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return count;
+    }
+    
     public static void main(String[] args) {
         PatientDAO patientDAO = new PatientDAO();
         int recordsPerPage = 4;
